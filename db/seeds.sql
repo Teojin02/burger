54a0fc9a4f2c87dee6dcd2b0c12095b608dec506
Your Name <you@example.com>
2020-10-15 21:34:25 -0700

    added db

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese burger');
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name, devour) VALUES ('Fish-O-Fillet', true);
+INSERT INTO burgers (name, devour) VALUES ('Double pounder', true);
+INSERT INTO burgers (name, devour) VALUES ('Double cheese burger', true);
+INSERT INTO burgers (name) VALUES ('Vegan burger');
